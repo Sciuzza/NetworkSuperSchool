ddf5fd511c42054a545febacdef340486a542a54
dbga_29 <dbga_29@dbga.local>
2017-01-18 12:39:26 +0100

    no message

--- Assets/Scripts/PlayerWeaponUse.cs
+++ Assets/Scripts/PlayerWeaponUse.cs
@@ -5,9 +5,33 @@ using System.Collections;
 public class PlayerWeaponUse : NetworkBehaviour
 {
     public Transform headTr;
-    public GameObject lineRenderer;
 
-	void Update ()
+    private const int NUMBER_OF_WEAPONS = 5;
+
+    [SyncVar]
+    public int selectedWeaponIndex = 0;
+    //-1 means infinite
+    // 0 cannot shoot
+    //
+    public SyncListInt ammoList;
+
+    public void Awake()
+    {
+        ammoList = new SyncListInt();
+        for (int i = 0; i < NUMBER_OF_WEAPONS; i++)
+        {
+            ammoList.Add(0);
+        }
+        ammoList[0] = -1;
+    }
+
+    public void ServerAddAmmo(int weaponIndex, int ammoAmount)
+    {
+
+    }
+
+
+    void Update ()
     {
         if (isLocalPlayer)
         {
@@ -21,26 +45,48 @@ public class PlayerWeaponUse : NetworkBehaviour
     [Command]
     public void CmdShoot(Vector3 targetPosition)
     {
-        RaycastHit hit;
-        Vector3 direction = targetPosition - transform.position;
-        if(Physics.Raycast(headTr.position, direction, out hit))
-        {
-            GameObject hitGo = hit.collider.gameObject;
-            PlayerState ps = hitGo.GetComponent<PlayerState>();
-            if (ps == null && hitGo.transform.parent != null) ps = hitGo.transform.parent.GetComponent<PlayerState>();
+        bool canShoot = false;
 
-            if (ps != null)
+        if (ammoList[selectedWeaponIndex] == -1)
+        {
+            canShoot = true;
+        }
+        else if (ammoList[selectedWeaponIndex] > 0)
+        {
+            canShoot = true;
+        }
+
+        if (canShoot)
+        {
+            RaycastHit hit;
+            Vector3 direction = targetPosition - transform.position;
+            if (Physics.Raycast(headTr.position, direction, out hit))
             {
-                Debug.DrawLine(headTr.position, hit.point, Color.green, 1f);
-                ps.ServerTakeDamage(10);
+                GameObject hitGo = hit.collider.gameObject;
+                PlayerState ps = hitGo.GetComponent<PlayerState>();
+                if (ps == null && hitGo.transform.parent != null) ps = hitGo.transform.parent.GetComponent<PlayerState>();
+
+                if (ps != null)
+                {
+                    Debug.DrawLine(headTr.position, hit.point, Color.green, 1f);
+                    ps.ServerTakeDamage(10);
+                    RpcShootFeedback(hit.point, Color.blue);
+                }
+                else
+                {
+                    Debug.DrawLine(headTr.position, hit.point, Color.red, 1f);
+                    RpcShootFeedback(hit.point, Color.white);
+                }
+             
             }
             else
             {
-                Debug.DrawLine(headTr.position, hit.point, Color.red, 1f);
+                RpcShootFeedback(headTr.forward * 100, Color.white);
             }
-            RpcShootFeedback(hit.point);
-        }
-    }  
+        }  
+    }
+
+    public GameObject lineRenderer;
 
     public IEnumerator DespawnShootFeedback(GameObject shootFeedback)
     {
@@ -49,11 +95,12 @@ public class PlayerWeaponUse : NetworkBehaviour
     }
 
     [ClientRpc]
-    public void RpcShootFeedback(Vector3 hitPoint)
+    public void RpcShootFeedback(Vector3 hitPoint, Color color)
     {
         GameObject shootSpawned = (GameObject)Instantiate(lineRenderer, headTr.position, Quaternion.identity);
-        shootSpawned.GetComponent<LineRenderer>().SetPosition(0, headTr.position);
+        shootSpawned.GetComponent<LineRenderer>().SetPosition(0, headTr.position - new Vector3(0,-0.3f,0));
         shootSpawned.GetComponent<LineRenderer>().SetPosition(1, hitPoint);
+        shootSpawned.GetComponent<LineRenderer>().SetColors(color, color);
         StartCoroutine(DespawnShootFeedback(shootSpawned));
     }
 }
