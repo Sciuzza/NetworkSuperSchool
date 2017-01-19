a1778c2a8fdde697385010bee16d20a6b8c12fd4
dbga_31 <dbga_31@dbga.local>
2017-01-19 13:38:07 +0100

    Added Max Score, Team Initialization, Changing Team Score (Switch - Case or For) and EndGameCheck method (without implementation)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,13 +12,16 @@ public class GameController : NetworkBehaviour {
 	// 1 - 4
 	public int m_NumberOfTeams = 0;
 
+	//Number of Max Score for DeathMatch and Team DeathMatch modalities
+	public int m_MaxDeathMatchScore = 40;
+
 	//Number of teams, with total score inside each box
 	public List <int> m_TotalTeamScoreList;
 	#endregion
 
 
 	#region GAME_CONTROLLER_MONO_BEHAVIOUR_METHODS
-	[Server]
+	[Server] //This is executed only by server
 	private void Awake () {
 
 		this.m_TotalTeamScoreList = new List <int> ();
@@ -28,14 +31,100 @@ public class GameController : NetworkBehaviour {
 
 
 	#region METHODS
-	public void ChangeTeamScore (short score, short team) {
+	[Server] //This is executed only by server
+	public void InitializeTeams (int teams) {
+
+		int i = -1;
 
 
+		if ((teams > 1) && (teams < 5))
+			this.m_NumberOfTeams = teams - 1;
+		
+		else {
+			
+			Debug.LogError (this.ToString () + ": Game Controller cannot set the right number of teams (i. e.: from 2 to 4)!");
+			return;
+
+		}
+
+
+		if (this.m_TotalTeamScoreList != null) {
+			
+			do {
+				
+				this.m_TotalTeamScoreList.Add (0);
+
+			} while (++i < this.m_NumberOfTeams);
+
+		} else {
+
+			Debug.LogWarning (this.ToString () + ": Game Controller has forced the initialization of the <<TotalTeamScoreList>>");
+
+			this.m_TotalTeamScoreList = new List <int> ();
+
+			do {
+
+				this.m_TotalTeamScoreList.Add (0);
+
+			} while (++i < this.m_NumberOfTeams);
+
+		}
 
 	}
 
 
-	//public void
+
+	[Server] //This is executed only by server
+	public void ChangeTeamScore (short score, short team) {
+
+		/*for (int i = 0; i <= m_NumberOfTeams; i++) {
+			
+			if (i == team) {
+				
+				this.m_TotalTeamScoreList [i] += score;
+				this.EndGameCheck ();
+				return;
+				
+			}
+			
+		}
+
+		Debug.LogError (this.ToString () + ": Game Controller cannot select a team while attempting specific total team score variation!");*/
+
+		switch (team) {
+
+		case 0:
+			this.m_TotalTeamScoreList [0] += score;
+			break;
+
+		case 1:
+			this.m_TotalTeamScoreList [1] += score;
+			break;
+
+		case 2:
+			this.m_TotalTeamScoreList [2] += score;
+			break;
+
+		case 3:
+			this.m_TotalTeamScoreList [3] += score;
+			break;
+
+		default:
+			Debug.LogError (this.ToString () + ": Game Controller cannot select a team while attempting specific total team score variation!");
+			break;
+
+		}
+
+		this.EndGameCheck ();
+
+	}
+
+
+	public void EndGameCheck () {
+
+
+
+	}
 	#endregion
 
 }
