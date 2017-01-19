0f69546640712a0e93c44503d7692c359f4279b3
dbga_39 <dbga_39@dbga.local>
2017-01-19 11:35:00 +0100

    Lobby system

---
+++ Assets/Scripts/LobbyPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyPlayer : NetworkLobbyPlayer
+{
+
+    public void Update()
+    {
+        if (isLocalPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (this.readyToBegin)
+                {
+                    SendNotReadyToBeginMessage();
+                }
+                else
+                {
+                    SendReadyToBeginMessage();
+                }
+                //this.readyToBegin = !readyToBegin;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MyLobbyNetworkManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyLobbyNetworkManager : NetworkLobbyManager
+{
+
+    // we need to override this to avoid the manager changing scene automatically
+    public override void OnLobbyServerPlayersReady()
+    {
+        Debug.Log("ALL READY");
+    }
+
+    void OnGUI()
+    {
+        // Check if everyone is ready
+        bool allReady = true;
+        for (int i = 0; i < lobbySlots.Length; i++)
+        {
+            if (!lobbySlots[i]) continue;
+            if (!lobbySlots[i].readyToBegin)
+            {
+                allReady = false;
+                break;
+            }
+        }
+
+        if (allReady)
+        {
+            if (GUILayout.Button("START"))
+            {
+                ServerChangeScene(playScene);
+            }
+        }
+    }
+
+}
