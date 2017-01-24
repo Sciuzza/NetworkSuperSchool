80566e45cd5543a1ee559521a84a30594b9b6b4e
dbga_22 <dbga_22@dbga.local>
2017-01-24 13:17:21 +0100

    -  Cambio arma - Aggiornamento Ui - Cambio colore arma - Quando è infinito l'arma spara

--- Assets/Scripts/PlayerWeaponUse.cs
+++ Assets/Scripts/PlayerWeaponUse.cs
@@ -5,10 +5,11 @@ using System.Collections;
 public class PlayerWeaponUse : NetworkBehaviour
 {
     public Transform weaponTr;
+    private GameObject weapon;
     const float WEAPON_RANGE = 100;
 
 
-    private const int NUMBER_OF_WEAPONS = 5;
+    private const int NUMBER_OF_WEAPONS = 7;
 
 
     // Synched on the server to clients
@@ -17,8 +18,43 @@ public class PlayerWeaponUse : NetworkBehaviour
     // >0 can shoot
     public SyncListInt ammoList = new SyncListInt();
 
-    [SyncVar]
+    [SyncVar(hook = "OnWeaponChange")]
     public int selectedWeaponIndex = 0;
+    
+
+    public void OnWeaponChange(int weaponIndex)
+    {
+        selectedWeaponIndex = weaponIndex;
+        MeshRenderer meshWeapon = weapon.GetComponent<MeshRenderer>();
+        switch (weaponIndex)
+        {
+            case 0:
+                meshWeapon.material.color = Color.white;
+                break;
+            case 1:
+                meshWeapon.material.color = Color.red;
+                break;
+            case 2:
+                meshWeapon.material.color = Color.green;
+                break;
+            case 3:
+                meshWeapon.material.color = Color.blue;
+                break;
+            case 4:
+                meshWeapon.material.color = Color.cyan;
+                break;
+            case 5:
+                meshWeapon.material.color = Color.yellow;
+                break;
+            case 6:
+                meshWeapon.material.color = Color.black;
+                break;
+        }
+
+
+
+
+    }
 
     public int CurrentWeaponAmmo
     {
@@ -27,6 +63,7 @@ public class PlayerWeaponUse : NetworkBehaviour
 
     public override void OnStartLocalPlayer()
     {
+        weapon = transform.Find("Head/Cube").gameObject;
         FindObjectOfType<AmmoUI>().pwu = this;
     }
 
@@ -53,20 +90,62 @@ public class PlayerWeaponUse : NetworkBehaviour
 
     public void ServerRefillAmmo()
     {
-        ammoList[0] = 20;
+        ammoList[0] = -1;
+        ammoList[1] = 1;
+        ammoList[2] = 2;
+        ammoList[3] = 3;
+        ammoList[4] = -1;
+        ammoList[5] = 5;
+        ammoList[6] = 6;
+    }
+
+    [Command]
+    public void CmdWeaponChange(int weaponIndex)
+    {
+        if ((ammoList[weaponIndex] > 0) || ammoList[weaponIndex] <0)
+        {
+            selectedWeaponIndex = weaponIndex;
+        }
     }
 
 
 
-
-
-    void Update ()
+    void Update()
     {
         if (isLocalPlayer)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                CmdShoot(weaponTr.position + weaponTr.forward*100);
+                CmdShoot(weaponTr.position + weaponTr.forward * 100);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                CmdWeaponChange(0);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                CmdWeaponChange(1);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                CmdWeaponChange(2);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                CmdWeaponChange(3);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                CmdWeaponChange(4);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                CmdWeaponChange(5);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                CmdWeaponChange(6);
             }
         }
     }
@@ -91,14 +170,14 @@ public class PlayerWeaponUse : NetworkBehaviour
         {
             RaycastHit hit;
             Vector3 direction = targetPosition - weaponTr.transform.position;
-            if(Physics.Raycast(weaponTr.position, direction, out hit,WEAPON_RANGE))
+            if (Physics.Raycast(weaponTr.position, direction, out hit, WEAPON_RANGE))
             {
                 GameObject hitGo = hit.collider.gameObject;
                 PlayerState ps = hitGo.GetComponent<PlayerState>();
                 if (ps == null && hitGo.transform.parent != null) ps = hitGo.transform.parent.GetComponent<PlayerState>();
 
                 if (ps != null)
-                { 
+                {
                     Debug.DrawLine(weaponTr.position, hit.point, Color.green, 1f);
                     ps.ServerTakeDamage(10, playerControllerId);
 
@@ -109,11 +188,11 @@ public class PlayerWeaponUse : NetworkBehaviour
                     Debug.DrawLine(weaponTr.position, hit.point, Color.red, 1f);
                     RpcShootFeedback(hit.point, Color.red);
                 }
-      
-             }
+
+            }
             else
             {
-                RpcShootFeedback(weaponTr.position + direction*WEAPON_RANGE, Color.red);
+                RpcShootFeedback(weaponTr.position + direction * WEAPON_RANGE, Color.red);
 
             }
         }
