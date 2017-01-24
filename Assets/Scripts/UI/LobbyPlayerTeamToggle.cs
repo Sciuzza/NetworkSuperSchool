ed26b35663273741dba06d65fc10833728fe8544
dbga_39 <dbga_39@dbga.local>
2017-01-24 12:13:39 +0100

    Lobby team to game

---
+++ Assets/Scripts/UI/LobbyPlayerTeamToggle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Networking;
+
+public class LobbyPlayerTeamToggle : NetworkBehaviour
+{
+    public short teamId;
+    public LobbyScore lobbyScore;
+
+    void OnStartLocalPlayer()
+    {
+        this.GetComponent<UnityEngine.UI.Toggle>().onValueChanged.AddListener(UpdateTeam);
+    }
+
+    void UpdateTeam(bool on)
+    {
+        if (on)
+        {
+            if (isLocalPlayer)
+            {
+                lobbyScore.CmdChangeTeam(teamId);
+            }
+        }
+    }
+}
