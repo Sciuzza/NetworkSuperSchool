c65ef37c779b722ff577c24f1f558d3b71259227
dbga_33 <dbga_33@dbga.local>
2017-01-25 13:20:12 +0100

    - Start boomerang

---
+++ Assets/Scripts/Weapons/BoomerangWeapon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System;
+
+public class BoomerangWeapon : AbstractWeapon
+{
+    private const float WEAPON_RANGE = 100;
+
+    public GameObject boomerangGo;
+
+    public override void Shoot(Vector3 targetPosition)
+    {
+        GameObject go = Instantiate(boomerangGo);
+        go.transform.position = Vector3.zero;
+        NetworkServer.Spawn(go);
+
+        go.transform.position = Vector3.Lerp(transform.position, transform.position + new Vector3(0, 0, 2), 500f);
+    }
+	
+	void Update ()
+    {
+                
+    }
+}
