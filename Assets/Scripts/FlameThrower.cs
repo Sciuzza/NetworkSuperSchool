411c1430762ec4f4e229e3ba9c45ca79ef2cb6a4
dbga_36 <dbga_36@dbga.local>
2017-01-25 15:07:35 +0100

    - Flame Thrower

---
+++ Assets/Scripts/FlameThrower.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class FlameThrower : AbstractWeapon
+{
+    public BoxCollider areaFireDamage;
+    public ParticleSystem fire;
+    public int damageFire = 5;
+
+    private void OnTriggerStay(Collider _other)
+    {
+        if (!isServer)
+            return;
+
+        PlayerState player = _other.gameObject.GetComponent<PlayerState>();
+
+        if (player != null)
+            player.ServerTakeDamage(damageFire, playerControllerId);
+    }
+    
+    public override void OnStartServer()
+    {
+        Destroy(fire);
+    }
+
+    public override void Shoot(Vector3 targetPosition)
+    {
+        areaFireDamage.enabled = true;
+
+        if (!isServer)
+            RpcFireFeedback();
+    }
+
+    [ClientRpc]
+    public void RpcFireFeedback()
+    {
+        StartCoroutine(BlazeCO(2));
+    }
+
+    private IEnumerator BlazeCO(float _seconds)
+    {
+        fire.Play();
+        yield return new WaitForSeconds(_seconds);
+        fire.Stop();
+    }
+}
