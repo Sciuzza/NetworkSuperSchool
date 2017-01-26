074db034c2a2cc4eb62c83049b4274070f7638eb
dbga_29 <dbga_29@dbga.local>
2017-01-26 11:02:41 +0100

    fixing weapons and resources

--- Assets/Scripts/PlayerWeaponUse.cs
+++ Assets/Scripts/PlayerWeaponUse.cs
@@ -44,7 +44,7 @@ public class PlayerWeaponUse : NetworkBehaviour
         //weapons[3] = this.gameObject.AddComponent<GranadeLauncherWeapon>();
         //weapons[4] = this.gameObject.AddComponent<BoomerangWeapon>();
         //weapons[5] = this.gameObject.AddComponent<DucklingWeapon>();
-        //weapons[6] = this.gameObject.AddComponent<RocketLauncherWeapon>();
+        weapons[6] = this.gameObject.AddComponent<RocketWeapon>();
 
         foreach(var weapon in weapons)
         {
