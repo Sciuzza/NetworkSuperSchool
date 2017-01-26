9274871641c77f42bcdc633f33f5f1ac444c4922
dbga_33 <dbga_33@dbga.local>
2017-01-26 12:16:34 +0100

    - Boomerang prefab

--- Assets/Scripts/Weapons/BoomerangAmmo.cs
+++ Assets/Scripts/Weapons/BoomerangAmmo.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class BoomerangAmmo : NetworkBehaviour
 {
     bool hasHit;
+    private Transform ammoTr;
     public Transform weaponTr;
     public short idPlayer;
 
@@ -12,12 +13,24 @@ public class BoomerangAmmo : NetworkBehaviour
     {
         float timeTransition = 0;
 
-        while ((weaponTr.position - (weaponTr.position + new Vector3(0, 0, 2))).magnitude > 0.01f)
+        while ((weaponTr.position - weaponTr.position + (direction / 2)).magnitude > 0.01f)
         {
             timeTransition += Time.deltaTime;  // bullet speed
 
             go.transform.position = Vector3.Lerp(weaponTr.position, weaponTr.position + (direction/2) , timeTransition);
             yield return 0;
         }
+ 
+        /*ammoTr = go.transform;
+        timeTransition = 0;
+        yield return new WaitForSeconds(1);
+
+        while ((ammoTr.position - weaponTr.position).magnitude > 0.01f)
+        {
+            timeTransition += Time.deltaTime;
+
+            go.transform.position = Vector3.Lerp(ammoTr.position, weaponTr.position, timeTransition);
+            yield return 0;
+        }*/
     }
 }
